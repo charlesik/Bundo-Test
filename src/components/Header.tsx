ea444bcaf9c7d943ed628fa298b399bde493f853
Charles Ilo <charlesikilo@gmail.com>
2025-03-30 23:32:27 +0100

    navigation and routing updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Logo } from "./svg";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
-import { ChevronDown, Menu, ShoppingCart, X } from "lucide-react";
+import { ChevronDown, ChevronRight, Menu, ShoppingCart, X } from "lucide-react";
 import {
   Drawer,
   DrawerClose,
@@ -13,43 +13,67 @@ import {
   DrawerTrigger,
 } from "./ui/drawer";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 function Header() {
   return (
     <div className="flex items-center h-[88px] gap-5 lg:justify-center justify-between border-y px-2">
-    <div className="flex gap-2 items-center">
-      <Drawer direction="right">
-        <DrawerTrigger asChild>
-          <Menu color="#34A853" size={"28px"} className="lg:hidden"/>
-        </DrawerTrigger>
-        <DrawerContent>
-          <div className="mx-auto w-full max-w-sm">
-            <DrawerHeader>
-              <DrawerTitle>
-                <div className="flex justify-between">
-                  <Logo />
-                  <div className="flex gap-1.5 items-center">
-                    <ShoppingCart color="#34A853" size={"28px"} />
-                    <DrawerClose asChild>
-                      <X color="#34A853" size={"28px"} />
-                    </DrawerClose>
+      <div className="flex gap-2 items-center">
+        <Drawer direction="right">
+          <DrawerTrigger asChild className="cursor-pointer">
+            <Menu color="#34A853" size={"28px"} className="lg:hidden" />
+          </DrawerTrigger>
+          <DrawerContent>
+            <div className="mx-auto w-full max-w-sm">
+              <DrawerHeader>
+                <DrawerTitle>
+                  <div className="flex justify-between">
+                    <Link href={"/"} className="cursor-pointer"><Logo/></Link>
+                    <div className="flex gap-1.5 items-center">
+                      <ShoppingCart color="#34A853" size={"28px"} />
+                      <DrawerClose asChild>
+                        <X color="#34A853" size={"28px"} />
+                      </DrawerClose>
+                    </div>
                   </div>
-                </div>
-              </DrawerTitle>
-              <DrawerDescription>
-              </DrawerDescription>
-            </DrawerHeader>
-            <div className="p-2">
-                <Button className="w-full bg-white border border-[#34A853] text-[#34A853] rounded-sm my-2">Sign In</Button>
-                <Button className="w-full bg-[#11381C] border border-[#34A853] text-white rounded-sm my-2">Sign In</Button>
-                <Button className="w-full bg-white border border-[#34A853] text-[#34A853] rounded-sm my-2">Vendors Stores</Button>
-
+                </DrawerTitle>
+                <DrawerDescription></DrawerDescription>
+              </DrawerHeader>
+              <div className="p-2">
+                <Button className="w-full bg-white border border-[#34A853] text-[#34A853] rounded-sm my-2">
+                  Sign In
+                </Button>
+                <Button className="w-full bg-[#11381C] border border-[#34A853] text-white rounded-sm my-2">
+                  Sign In
+                </Button>
+                <Link href={"/businesses"}><Button className="w-full bg-white border border-[#34A853] text-[#34A853] rounded-sm my-2">
+                  Vendors Stores
+                </Button></Link>
+              </div>
+              <div className="px-2">
+                <p className="font-[500] text-[18px] text-[#34A853] my-3">
+                  Categories
+                </p>
+                <Link href={"/"}>
+                  {" "}
+                  <div className="flex justify-between hover:bg-[#34A8530D] py-3">
+                    <p>Home</p>
+                    <ChevronRight />
+                  </div>
+                </Link>
+                <Link href={"/businesses"}>
+                  {" "}
+                  <div className="flex justify-between hover:bg-[#34A8530D] py-3">
+                    <p>Add Business</p>
+                    <ChevronRight />
+                  </div>
+                </Link>
+              </div>
             </div>
-            
-          </div>
-        </DrawerContent>
-      </Drawer>
-      <Logo /></div>
+          </DrawerContent>
+        </Drawer>
+        <Link href={"/"} className="cursor-pointer"><Logo/></Link>
+      </div>
       <div className="lg:flex items-center gap-1 hidden ">
         <p>Categories </p>
         <div className="bg-[#11381C] rounded-full flex items-center justify-center">
@@ -70,7 +94,7 @@ function Header() {
       <button className="bg-[#11381C] text-white font-bold p-3 rounded-lg h-[48px] w-[155px] hidden lg:block">
         Sell on Bundo
       </button>
-      <button className="text-[#34A853] border border-[#34A853]  p-3 rounded-lg h-[48px] w-[155px] hidden lg:block">{`Vendor's Store`}</button>
+      <Link href={"/businesses"}><button className="text-[#34A853] border border-[#34A853]  p-3 rounded-lg h-[48px] w-[155px] hidden lg:block">{`Vendor's Store`}</button></Link>
       <div className="flex items-center gap-2">
         <Avatar>
           <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
